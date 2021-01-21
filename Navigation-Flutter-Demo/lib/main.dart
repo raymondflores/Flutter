3673190525e33884529aea575c60706eed8ecfcf
Raymond Flores <raymondfflores@gmail.com>
2021-01-20 20:00:33 -0600

    save progress

---
+++ Navigation-Flutter-Demo/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screen0.dart';
+import 'screen1.dart';
+import 'screen2.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) {
+          return Screen0();
+        },
+        '/first': (context) {
+          return Screen1();
+        },
+        '/second': (context) {
+          return Screen2();
+        }
+      },
+    );
+  }
+}
